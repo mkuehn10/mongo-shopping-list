313211546c7dab05aeed31486ab3aea51dacb70f
Michael Kuehn <mkuehn10@gmail.com>
2016-08-10 13:50:43 +0000

    fix get test

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     (process.env.NODE_ENV === 'production' ?
         'mongodb://mkuehn10:mkuehn10@ds145245.mlab.com:45245/mkuehn10' :
-        'mongodb://mkuehn10:mkuehn10@ds145245.mlab.com:45245/mkuehn10');
+        'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
